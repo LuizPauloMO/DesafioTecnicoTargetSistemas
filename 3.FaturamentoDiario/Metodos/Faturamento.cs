fe915368717faf2951f24a3817b23a2b1fb51ced
Luiz Paulo <lpmendesdeoliveira@hotmail.com>
2025-05-12 12:04:17 -0300

    Incluindo a questao 03 na Solution

---
+++ 3.FaturamentoDiario/Metodos/Faturamento.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3.FaturamentoDiario.Metodos
+{
+    public class Faturamento
+    {
+       public List<Faturamento_Diario>? FaturamentoDiario {  get; set; }
+    }
+}
---
+++ 3.FaturamentoDiario/Metodos/Faturamento_Diario.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3.FaturamentoDiario.Metodos
+{
+    public class Faturamento_Diario
+    {
+        public Int32 Dia { get; set; }
+        public Double Valor { get; set; }
+    }
+}
