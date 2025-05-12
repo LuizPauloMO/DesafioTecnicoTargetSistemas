cf0a49422f3203b1d6ba717ad523a16f0b747635
Luiz Paulo <lpmendesdeoliveira@hotmail.com>
2025-05-12 16:00:00 -0300

    Questão n05 - Inversor de String finalizada com sucesso

---
+++ 5.InversorDeString/Program.cs
@@ -0,0 +1,27 @@
+﻿String texto = "Fernanda Montenegro";
+
+Console.WriteLine("Digite um texto e eu vou invertê-lo, ou enter para sair: ");
+texto=Console.ReadLine()??String.Empty;
+
+if (!String.IsNullOrEmpty(texto))
+{
+    String TextoInvertido = InverteString(texto);
+    Console.WriteLine("Seu texto invertido: "+TextoInvertido.ToUpper());
+    Console.ReadKey();
+}
+else
+{
+    Console.WriteLine("Tchauuuu......");
+    Console.ReadKey();
+}
+
+String InverteString(String texto)
+{
+   int len=texto.Length;
+    char[] ch=new char[len];
+    for(int i=0; i<len; i++)
+    {
+        ch[i] = texto[len - 1 - i];
+    }
+    return new string(ch);
+}
